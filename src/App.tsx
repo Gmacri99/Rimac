b3ccfda3f4df2b868595de52708ac71617213fc7
Gmacri99 <macrigianny2021@gmail.com>
2025-08-12 01:39:48 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import { Seguro } from './pages/Seguro'
 import { Planes } from './pages/Planes'
